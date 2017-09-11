0406cabe064ae2ff4d1a2947d7cd07399938ff9e
Near Lg <nearlg@gmail.com>
2017-09-11 22:45:33 +0200

    Fix put by patch for all routes

--- routes/sensor.ts
+++ routes/sensor.ts
@@ -12,14 +12,14 @@ export function routes(server: restify.Server, mainPath: string = ''): void{
         .catch(err => handleErrors(err, next));
     });
 
-    server.put(mainPath, (req, res, next)=>{
+    server.patch(mainPath, (req, res, next)=>{
         sensorValidator.validate(req.body, true)
         .then(sensor => Middleware.updateSensor(sensor))
         .then(sensor => handleJsonData(sensor, res, next))
         .catch(err => handleErrors(err, next));
     });
 
-    server.put(mainPath + '/:id', (req, res, next)=>{
+    server.patch(mainPath + '/:id', (req, res, next)=>{
         sensorValidator.validate(req.body)
         .then(sensor => Middleware.updateSensorById(req.params.id, sensor))
         .then(sensor => handleJsonData(sensor, res, next))
