d1a9b1a4599d7a1a9ebf571eb9604ed746d82e58
Near Lg <nearlg@gmail.com>
2017-09-11 07:44:56 +0200

    #4 Fix routes with queries

--- routes/helpers.ts
+++ routes/helpers.ts
@@ -5,6 +5,22 @@ import { InternalServerError } from "restify-errors";
 
 let errorHandlers: IErrorHandler[] = new Array<IErrorHandler>();
 
+export function checkQuery(query: string[], querySended: object)
+: Promise<void | Error> {
+    let error: Error = new Error();
+    if(!querySended) {
+        error.name = 'MissingParameterError';
+        error.message ='Missing query parameter';
+        return Promise.reject(error);
+    }
+    let element: string = Object.keys(querySended).find(q => {
+        return q !in query;
+    });
+    error.name = 'InvalidArgumentError';
+    error.message = "Query " + element + " is not valid";
+    return element === undefined? Promise.resolve() : Promise.reject(error);
+}
+
 export function handleJsonData(data: any, res: Response, next: Next, status?: number): void {
     dataHandler.handleJson(data, res, next, status);
 }
