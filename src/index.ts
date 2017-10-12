da67314b79283e6126358a0fd565d2ee21b1e518
Near Lg <nearlg@gmail.com>
2017-10-12 18:00:52 +0200

    #16 Add library @types/restify-cors-middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as Config from "../config/config";
 import * as restify from "restify";
-import corsMiddleware from "restify-cors-middleware";
+import * as corsMiddleware from "restify-cors-middleware";
 
 import mongoose = require("mongoose");
 
@@ -30,7 +30,7 @@ addErrorHandler(DataErrorHandler);
 addErrorHandler(MongooseErrorHandler);
 
 // Allow CORS
-const cors = corsMiddleware({
+const cors: corsMiddleware.CorsMiddleware = corsMiddleware({
   origins: ['*'],
   allowHeaders: ['API-Token'],
   exposeHeaders: ['API-Token-Expiry']
