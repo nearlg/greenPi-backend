e111a6104b8a1cc3a3d9b66edd1bf275e5b2d7ea
Near lg <nearlg@gmail.com>
2018-10-09 12:52:52 +0200

    Fix: config/server variables
    Now it uses process.env.SERVER_NAME = and the value 'greenpi-backend' by default
    and process.env.API_VERSION = with the value '0.0.0' by default

--- config/server.ts
+++ config/server.ts
@@ -1,3 +1,3 @@
 export const PORT: number = Number(process.env.PORT) || 8080;
-export const NAME: string = process.env.SERVER_NAME;
-export const VERSION: string = '0.0.0';
+export const NAME: string = process.env.SERVER_NAME || 'greenpi-backend';
+export const VERSION: string = process.env.API_VERSION || '0.0.0';
