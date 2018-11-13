8259e7f17f6409f770686cdeb54b6034f1a70041
Near lg <nearlg@gmail.com>
2018-11-13 21:57:08 +0100

    Add: security config
    And set the environments variables in the Dockerfile

--- config/security.ts
+++ config/security.ts
@@ -1,1 +1,2 @@
-export const BCRYPT_SALT_ROUNDS: number = 10;
+export const BCRYPT_SALT_ROUNDS = Number(process.env.SECURITY_BCRYPT_SALT_ROUNDS);
+export const JWT_SECRET = process.env.SECURITY_JWT_SECRET;
