b5e55c8302fc05d47a831b9f15820fe5fdbde027
Near lg <nearlg@gmail.com>
2018-11-20 18:54:07 +0100

    Add: google controller, service and config file

---
+++ controllers/google.ts
@@ -0,0 +1,33 @@
+import { Request, Response, Next } from 'restify';
+import { userRepository } from '../models/database/repository/implementation/mongoose4/user-repository';
+import { createToken } from '../services/jwt-service';
+import * as userValidator from '../validation/user';
+import { verify } from '../services/google-auth.service';
+import { IUser } from '../models/interface/user';
+import { RoleName } from '../services/authz-service/role-name';
+import { handleJsonData, handleErrors } from './helpers';
+
+export function signInGoogle(req: Request, res: Response, next: Next) {
+    const idToken: string = req.body.idToken;
+    verify(idToken)
+    .then(payload => {
+        const userId = payload.sub;
+        return userRepository.findByGoogleId(userId)
+        .catch(() => {
+            const newUser = <IUser>{
+                name: payload.name,
+                email: payload.email,
+                roleName: RoleName.Observer,
+                googleId: payload.sub
+            };
+            return userValidator.validate(newUser)
+            .then(userRepository.create);
+        });
+    })
+    .then(user => {
+        const token = createToken(user);
+        return Promise.resolve(token);
+    })
+    .then(token => handleJsonData(req, res, next, token))
+    .catch(err => handleErrors(err, next));
+}
--- routes/users.ts
+++ routes/users.ts
@@ -1,10 +1,11 @@
 import * as restify from 'restify';
 import * as controller from '../controllers/user';
+import * as gController from '../controllers/google';
 
 export function routes(server: restify.Server, mainPath: string = ''): void{
     // User profile
     server.post(mainPath + '/profile/sign-in/local', controller.signInLocal);
-    server.post(mainPath + '/profile/sign-in/google', controller.signInGoogle);
+    server.post(mainPath + '/profile/sign-in/google', gController.signInGoogle);
     server.post(mainPath + '/profile', controller.signUp);
     server.get(mainPath + '/profile', controller.getProfile);
     server.patch(mainPath + '/profile', controller.editProfile);
