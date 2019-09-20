881c98b854d01289e404f17f002b8a3184f154c4
Gabriel Mateos <gmdcwork@gmail.com>
2019-09-20 17:07:06 +0200

    chore: plugin authorization use async

--- src/plugins/authorization.ts
+++ src/plugins/authorization.ts
@@ -1,44 +1,54 @@
-import { Request, Next, Response} from 'restify';
-import { verifyTokenFromRequest } from '../services/jwt.service';
-import { handleErrors } from '../controllers/helpers';
-import { RoleName } from '../services/authz.service/role-name';
-import { authzService } from '../services/authz.service';
-import { userRepository } from '../repositories';
+import { Request, Next, Response } from "restify";
+import { verifyTokenFromRequest } from "../services/jwt.service";
+import { handleErrors } from "../controllers/helpers";
+import { RoleName } from "../services/authz.service/role-name";
+import { authzService } from "../services/authz.service";
+import { userRepository } from "../repositories";
+
+/**
+ * Get the role name depending on the authorization header key
+ * If authorization header key exist, get the role name from the user
+ * If authorization header key does not exist, the role name is 'NonRegistered'
+ * @param req
+ */
+async function getRoleNameByRequest(req: Request) {
+  const isAuthorization = req.headers.authorization ? true : false;
+  if (!isAuthorization) {
+    return RoleName.NonRegistered;
+  }
+  const validToken = verifyTokenFromRequest(req);
+  const userId = validToken["sub"];
+  const roleName = await userRepository.getRoleName(userId);
+  return roleName;
+}
 
 /**
  * Check if the user role can have access to the path
  * @param req
  * @param token
  */
-function requestIsAuthorized(req: Request)
-: Promise<void> {
-    const method = req.method;
-    const path = req.getRoute().path.toString();
-    return Promise.resolve(!req.headers.authorization)
-    // Get the role name depending on the authorization header key
-    // If authorization header key exist, get the role name from the user
-    // If authorization header key does not exist, the role name is 'NonRegistered'
-    .then(noAuthorization => {
-        return noAuthorization ? RoleName.NonRegistered :
-        verifyTokenFromRequest(req)
-        .then(validToken => validToken.sub)
-        .then(userRepository.getRoleName);
-    })
-    // If the rolename is NOT authorize for the giving path and method,
-    // It creates an error
-    .then(roleName => authzService.isAuthorized(roleName, path, method))
-    .then(isAuthorized => {
-        if(!isAuthorized) {
-            const err = new Error();
-            err.name = !req.headers.authorization ?
-            'UnauthorizedError' : 'NotAuthorizedError';
-            return Promise.reject(err);
-        }
-    });
+async function requestIsAuthorized(req: Request) {
+  const method = req.method;
+  const path = req.getRoute().path.toString();
+  // Get the role name depending on the authorization header key
+  let roleName = await getRoleNameByRequest(req);
+  // If the rolename is NOT authorize for the giving path and method,
+  // It creates an error
+  const isAuthorized = await authzService.isAuthorized(roleName, path, method);
+  if (!isAuthorized) {
+    const err = new Error();
+    err.name = !req.headers.authorization
+      ? "UnauthorizedError"
+      : "NotAuthorizedError";
+    throw err;
+  }
 }
 
-export function requestAuthz(req: Request, res: Response, next: Next) {
-    requestIsAuthorized(req)
-    .then(() => next())
-    .catch(err => handleErrors(next, err));
+export async function requestAuthz(req: Request, res: Response, next: Next) {
+  try {
+    await requestIsAuthorized(req);
+    next();
+  } catch (err) {
+    handleErrors(next, err);
+  }
 }
