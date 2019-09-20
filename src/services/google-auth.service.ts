b320a65a6392294622c5b621e784224ff6818300
Gabriel Mateos <gmdcwork@gmail.com>
2019-09-20 17:04:26 +0200

    chore: services use async

--- src/services/google-auth.service.ts
+++ src/services/google-auth.service.ts
@@ -1,27 +1,34 @@
 import * as GoogleConfig from "../config/google";
 
-const { OAuth2Client } = require('google-auth-library');
+const { OAuth2Client } = require("google-auth-library");
 
 const clientSecret = GoogleConfig.GOOGLE_CLIENT_SECRET;
 const clientId = GoogleConfig.GOOGLE_CLIENT_ID;
 const redirectUris = [GoogleConfig.GOOGLE_REDIR_URL];
 
-function getAuthUrl(): Promise<string> {
-    const oAuth2Client = new OAuth2Client(
-        clientId, clientSecret, redirectUris[0]);
+async function getAuthUrl() {
+  const oAuth2Client = new OAuth2Client(
+    clientId,
+    clientSecret,
+    redirectUris[0]
+  );
 
-    const authUrl: string = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: ['profile', 'email'],
-    });
-    return Promise.resolve(authUrl);
+  const authUrl: string = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: ["profile", "email"]
+  });
+  return authUrl;
 }
 
-function getIdToken(code: string): Promise<string> {
-    const oAuth2Client = new OAuth2Client(
-        clientId, clientSecret, redirectUris[0]);
-    return oAuth2Client.getToken(code)
-    .then(res => res.tokens.id_token);
+async function getIdToken(code: string) {
+  const oAuth2Client = new OAuth2Client(
+    clientId,
+    clientSecret,
+    redirectUris[0]
+  );
+  const res = await oAuth2Client.getToken(code);
+  const idToken = res.tokens.id_token;
+  return idToken;
 }
 
 /**
@@ -29,19 +36,17 @@ function getIdToken(code: string): Promise<string> {
  * The payload contains, the userId, in payload.sub
  * @param idToken
  */
-function verify(idToken: string): Promise<any> {
-    const client = new OAuth2Client(clientId);
-    return client.verifyIdToken({
-        idToken: idToken,
-        // Specify the CLIENT_ID of the app that accesses the backend
-        audience: clientId,
-        // Or, if multiple clients access the backend:
-        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-    }).then(ticket => ticket.getPayload());
+async function verify(idToken: string) {
+  const client = new OAuth2Client(clientId);
+  const ticket = await client.verifyIdToken({
+    idToken: idToken,
+    // Specify the CLIENT_ID of the app that accesses the backend
+    audience: clientId
+    // Or, if multiple clients access the backend:
+    //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const ticketPayload = ticket.getPayload();
+  return ticketPayload;
 }
 
-export {
-    verify,
-    getAuthUrl,
-    getIdToken
-};
+export { verify, getAuthUrl, getIdToken };
--- src/services/pumps-sio.service.ts
+++ src/services/pumps-sio.service.ts
@@ -1,74 +1,69 @@
-import { Subscriber } from 'rxjs';
+import { Subscriber } from "rxjs";
 
-import { PumpHistorical } from '../models/interface/pump-historical';
+import { PumpHistorical } from "../models/interface/pump-historical";
 
-import { pumpHistoricalRepository } from '../repositories';
-import { SIOService } from './sio.service';
+import { pumpHistoricalRepository } from "../repositories";
+import { SIOService } from "./sio.service";
 
 const enum NamespaceNames {
-    LastPumpHistoricals = '/pumps/last-historicals'
-};
+  LastPumpHistoricals = "/pumps/last-historicals"
+}
 
 const enum EventNames {
-    LastHistoricals = 'last-historicals',
-    GetLastHistoricals = 'get-last-historicals'
-};
+  LastHistoricals = "last-historicals",
+  GetLastHistoricals = "get-last-historicals"
+}
 
 export class PumpsSIOService implements SIOService {
+  private io: SocketIO.Server;
+  private lastPumpHistoricals$: Subscriber<PumpHistorical>;
 
-    private io: SocketIO.Server;
-    private lastPumpHistoricals$: Subscriber<PumpHistorical>;
+  constructor(io: SocketIO.Server) {
+    this.io = io;
+    this.setUpSubscribers();
+  }
 
-    constructor(io: SocketIO.Server) {
-        this.io = io;
-        this.setUpSubscribers();
-    }
+  async emitLastPumpHistorical(
+    pumpHistorical: PumpHistorical
+  ): Promise<PumpHistorical> {
+    this.lastPumpHistoricals$.next(pumpHistorical);
+    return pumpHistorical;
+  }
 
-    emitLastPumpHistorical(pumpHistorical: PumpHistorical): Promise<PumpHistorical> {
-        this.lastPumpHistoricals$.next(pumpHistorical);
-        return Promise.resolve(pumpHistorical);
-    }
+  listen() {
+    this.listenLastPumpHistoricals();
+  }
 
-    listen() {
-        this.listenLastPumpHistoricals();
-    }
+  private setUpSubscribers() {
+    // Last pump historicals subscriber
+    const namespace = this.io.of(NamespaceNames.LastPumpHistoricals);
+    this.lastPumpHistoricals$ = new Subscriber(o => {
+      namespace.emit(EventNames.LastHistoricals, [o]);
+    });
+    // Other subscriber here
+  }
 
-    private setUpSubscribers() {
-        // Last pump historicals subscriber
-        const namespace = this.io.of(NamespaceNames.LastPumpHistoricals);
-        this.lastPumpHistoricals$ = new Subscriber(
-            o => {
-                namespace.emit(EventNames.LastHistoricals, [o]);
-            }
+  private listenLastPumpHistoricals() {
+    // Create the namespace
+    const namespace = this.io.of(NamespaceNames.LastPumpHistoricals);
+    // Listen any new connection for that namespace
+    namespace.on("connection", socket => {
+      // Set the events here.
+
+      // 'EventNames.GetLastHistoricals' event gets all the
+      // last pump historicals explicitycally
+      socket.on(EventNames.GetLastHistoricals, async data => {
+        // if there is any pump id,
+        // it gets all the pump id's from the system
+        let pumpIds: string[] = data.pumpIds || [];
+        if (!Array.isArray(pumpIds)) {
+          pumpIds = [];
+        }
+        const historicalPumps = await pumpHistoricalRepository.findLastsByPumpIds(
+          pumpIds
         );
-        // Other subscriber here
-    }
-
-    private listenLastPumpHistoricals() {
-        // Create the namespace
-        const namespace = this.io.of(NamespaceNames.LastPumpHistoricals);
-        // Listen any new connection for that namespace
-        namespace.on('connection', socket =>  {
-            // Set the events here.
-
-            // 'EventNames.GetLastHistoricals' event gets all the
-            // last pump historicals explicitycally
-            socket.on(EventNames.GetLastHistoricals, data => {
-                // if there is any pump id,
-                // it gets all the pump id's from the system
-                return new Promise(resolve => {
-                    let pumpIds: string[] = data.pumpIds || [];
-                    if (!Array.isArray(pumpIds)) {
-                        pumpIds = [];
-                    }
-                    resolve(pumpHistoricalRepository
-                    .findLastsByPumpIds(pumpIds));
-                })
-                .then(historicalPumps => {
-                    socket.emit(EventNames.LastHistoricals, historicalPumps);
-                });
-            });
-
-        });
-    }
+        return socket.emit(EventNames.LastHistoricals, historicalPumps);
+      });
+    });
+  }
 }
