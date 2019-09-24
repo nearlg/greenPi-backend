1b8329d43a09c700821071432af62ad367caa2f7
Gabriel Mateos <gmdcwork@gmail.com>
2019-09-24 16:38:16 +0200

    fix: new-version-script

--- new-version.sh
+++ new-version.sh
@@ -6,3 +6,11 @@
 # fi
 BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
+if [ "$TRAVIS_BRANCH" != "develop" ]; then
+    exit 0
+fi
+echo "HERE!!"
+# BRANCH="feature/major/something"
+if [[ "$BRANCH" =~ feature\/major\/.+ ]]; then echo "MAJOR + 1";
+elif [[ "$BRANCH" =~ feature\/minor\/.+ ]]; then echo "MINOR + 1";
+else echo "PATCH + 1"; fi
