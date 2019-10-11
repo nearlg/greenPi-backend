a923ebf523606a0f07b88dcba0c3a6abf8c9624f
Gabriel Mateos <gmdcwork@gmail.com>
2019-10-11 17:32:26 +0200

    fix: server name/version is the value saved in package.json

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,3 +1,5 @@
+import { name, version } from "../../package.json";
+
 export const PORT: number = Number(process.env.PORT) || 8080;
-export const NAME: string = process.env.SERVER_NAME || 'greenpi-backend';
-export const VERSION: string = process.env.API_VERSION || '0.0.0';
+export const NAME: string = process.env.SERVER_NAME || name;
+export const VERSION: string = version;
