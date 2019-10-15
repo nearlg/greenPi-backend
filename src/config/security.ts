c522dfdc674b38d5771d2cb2845e1745ad3e996e
Gabriel Mateos <gmdcwork@gmail.com>
2019-10-15 10:42:01 +0200

    fix: security config set BCRYPT_SALT_ROUNDS = 10 by defaut

--- src/config/security.ts
+++ src/config/security.ts
@@ -1,2 +1,4 @@
-export const BCRYPT_SALT_ROUNDS = Number(process.env.SECURITY_BCRYPT_SALT_ROUNDS);
+export const BCRYPT_SALT_ROUNDS = process.env.SECURITY_BCRYPT_SALT_ROUNDS
+  ? Number(process.env.SECURITY_BCRYPT_SALT_ROUNDS)
+  : 10;
 export const JWT_SECRET = process.env.SECURITY_JWT_SECRET;
