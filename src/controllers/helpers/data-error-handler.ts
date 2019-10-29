52d00e06bbb81760727a99cbaa9ed2d6dec0641d
Gabriel Mateos <gmdcwork@gmail.com>
2019-10-29 11:55:57 +0100

    fix: data-error-handler handleValidationError function

--- src/controllers/helpers/data-error-handler.ts
+++ src/controllers/helpers/data-error-handler.ts
@@ -1,32 +1,29 @@
-import { ErrorHandler } from './interface/error-handler';
-import { Next } from 'restify';
-import { NotFoundError, BadRequestError } from 'restify-errors';
-import { ValidationError } from 'mongoose';
+import { ErrorHandler } from "./interface/error-handler";
+import { Next } from "restify";
+import { NotFoundError, BadRequestError } from "restify-errors";
 
 class DataErrorHandler implements ErrorHandler {
-
-    handleError(next: Next, err: Error): boolean {
-        switch(err.name) {
-            case 'DataNotFoundError':
-                this.handleDataNotFoundError(next, err);
-                return true;
-            case 'DataValidationError':
-                this.handleValidationError(next, err);
-                return true;
-        }
-        return false;
+  handleError(next: Next, err: Error): boolean {
+    switch (err.name) {
+      case "DataNotFoundError":
+        this.handleDataNotFoundError(next, err);
+        return true;
+      case "DataValidationError":
+        this.handleValidationError(next, err);
+        return true;
     }
+    return false;
+  }
 
-    private handleValidationError(next: Next, err: Error) {
-        const validationError: ValidationError = <ValidationError>err;
-        const error: BadRequestError = new BadRequestError(validationError);
-        return next(error);
-    }
+  private handleValidationError(next: Next, err: Error) {
+    const error: BadRequestError = new BadRequestError(err);
+    return next(error);
+  }
 
-    private handleDataNotFoundError(next: Next, err: Error) {
-        const error: NotFoundError = new NotFoundError(err);
-        return next(error);
-    }
+  private handleDataNotFoundError(next: Next, err: Error) {
+    const error: NotFoundError = new NotFoundError(err);
+    return next(error);
+  }
 }
 
 export const errorHandler: DataErrorHandler = new DataErrorHandler();
