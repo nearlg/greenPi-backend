bfd6ce5ec3dd6f267a91c5677b51aa63aac8b7a7
Gabriel Mateos <gmdcwork@gmail.com>
2019-11-04 11:43:13 +0100

    add: ENVIRONMENT server config variable
    'development' value by default

--- src/config/server.ts
+++ src/config/server.ts
@@ -3,3 +3,4 @@ import { name, version } from "../../package.json";
 export const PORT: number = Number(process.env.PORT) || 8080;
 export const NAME: string = process.env.SERVER_NAME || name;
 export const VERSION: string = version;
+export const ENVIRONMENT: string = process.env.NODE_ENV || "development";
