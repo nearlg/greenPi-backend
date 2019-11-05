e5bb1fe3eb6a928b7c629c4897f230a9fd5fead7
Gabriel Mateos <gmdcwork@gmail.com>
2019-11-05 10:16:48 +0100

    add: rules field to 'model'

--- src/models/interfaces/model.ts
+++ src/models/interfaces/model.ts
@@ -1,6 +1,7 @@
 import { RoleName } from "../role-name";
 
 export interface Model<T> {
+  rules: Map<string, Set<string>>;
   findAll(roleName: RoleName): Promise<T[]>;
   create(roleName: RoleName, document: T): Promise<T>;
   update(roleName: RoleName, document: T): Promise<T>;
