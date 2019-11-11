c37714a8158ac6002d50ba11cb11a7ea94f9bb3d
Gabriel Mateos <gmdcwork@gmail.com>
2019-11-11 11:02:26 +0100

    add: ALLOW_CROSS_ORIGIN server config variable

--- src/config/server.ts
+++ src/config/server.ts
@@ -4,3 +4,6 @@ export const PORT: number = Number(process.env.PORT) || 8080;
 export const NAME: string = process.env.SERVER_NAME || name;
 export const VERSION: string = version;
 export const ENVIRONMENT: string = process.env.NODE_ENV || "development";
+export const ALLOW_CROSS_ORIGIN: boolean = process.env.ALLOW_CROSS_ORIGIN ?
+  Boolean(process.env.ALLOW_CROSS_ORIGIN) :
+  ENVIRONMENT === "development";
