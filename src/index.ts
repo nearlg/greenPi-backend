a2985291dbbeaedd36ebafadcafd0c39dcf055ac
Gabriel Mateos <gmdcwork@gmail.com>
2019-11-19 13:49:57 +0100

    chore: remove unnecessary comments

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,14 @@
-import * as Config from "./config";
-import * as restify from "restify";
+import * as Config from './config';
+import * as restify from 'restify';
 
-import mongoose = require("mongoose");
+import mongoose = require('mongoose');
 
-import { socketIOService } from "./services/sio/socket-io.service";
-import { setApiRoute } from "./graphql";
-import { requestAuth } from "./plugins/authentication";
-import { allowCrossOrigin } from "./plugins/allow-cross-origin";
+import { socketIOService } from './services/sio/socket-io.service';
+import { setApiRoute } from './graphql';
+import { requestAuth } from './plugins/authentication';
+import { allowCrossOrigin } from './plugins/allow-cross-origin';
 
-// // Configure database
-// mongoose.Promise = Promise;
-// // const options    = {promiseLibrary: Promise};
-// mongoose.connect(Config.Database.URI, { useMongoClient: true });
-
-// mongoose.Promise = Promise;
-// const options    = {promiseLibrary: Promise};
+// Configure database
 const mongooseOptions = <mongoose.ConnectionOptions>{
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -37,13 +31,13 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 server.use(requestAuth);
-if(Config.Server.ALLOW_CROSS_ORIGIN) {
+if (Config.Server.ALLOW_CROSS_ORIGIN) {
   server.use(allowCrossOrigin);
 }
 
 // GraphQL
-setApiRoute(server, "/graphql");
+setApiRoute(server, '/graphql');
 
 server.listen(Config.Server.PORT, function() {
-  console.log("%s listening at %s", server.name, server.url);
+  console.log('%s listening at %s', server.name, server.url);
 });
